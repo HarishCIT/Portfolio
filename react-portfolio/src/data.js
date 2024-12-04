015cef76d332665905b5ec1281e3f9311dc27bc5
HarishCIT <harishg.it2021@citchennai.net>
2024-12-04 21:13:38 +0530

    first

---
+++ react-portfolio/src/data.js
@@ -0,0 +1,68 @@
+export const config = {
+  portfolio: {
+    portfolioLogo: 'Portfolio',
+    projectCard: {
+      cards: [
+        {
+          heading: 'Project pizza app',
+          description:
+            'A pizza ordering platform with user and admin login, registration, email verification, and password recovery. Users can easily add or customize pizzas to their cart, make payments via Razorpay, and track order status in "My Orders."',
+          animationDelay: '300',
+          buttonLink: 'https://pizza-delivery-app-e1273.web.app/',
+          buttonText: 'CHECK OUT',
+        },
+        
+        {
+          heading: 'Under Construction',
+          description: '</>',
+          animationDelay: '1100',
+          buttonText: '',
+        },
+      ],
+    },
+    internships: {
+      internship: [
+        {
+          companyName: 'Company Name',
+          role: 'Front-end Developer',
+          introText: '',
+          duration: '29/03/2022 - 29/05/2022',
+          keyPointOne: 'Built front-end of multiple website using ReactJS',
+          keyPointTwo:
+            'Add few lines here....',
+          keyPointThree: 'Add few lines here... if requried Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis voluptas dolo Perspiciatis voluptas do.',
+          keyPointFour:
+            'Add few lines here... if requried Lorem, ipsum dolor sit amet consectetur adipisicing architecto molestiae.',
+          offsetValue: '0',
+        },
+        {
+          companyName: 'Company Name',
+          role: 'Front-end Developer',
+          introText: '',
+          duration: '14/06/2021 - 14/07/2021',
+          keyPointOne: 'Built front-end of multiple website using ReactJS.',
+          keyPointTwo: 'Add few lines here... if requried',
+          keyPointThree:
+            'Add few lines here... if requried',
+          keyPointFour:
+            'Add few lines here... if requried Lorem, ipsum dolor sit amet consectetur adipisicing cto molestiae.',
+          offsetValue: '-1000',
+        },
+        {
+          companyName: 'Company Name',
+          role: 'Front-end Developer',
+          introText: '',
+          duration: '10/09/2020 - 10/11/2020',
+          keyPointOne:
+            'Add few lines here... if requried',
+          keyPointTwo: 'Add few lines here... if requried',
+          keyPointThree: 'Add few lines here... if requried',
+          keyPointFour: '',
+          offsetValue: '-1000',
+        },
+      ],
+    },
+  },
+};
+
+export default config;
